f92a7ec560c7c98f00713d314d5db250b25fd01e
kenyc <d8956303@gmail.com>
2023-09-19 15:01:30 +0800

    加入powershell一次執行功能

---
+++ all_pdf.ps1
@@ -0,0 +1,14 @@
+# PowerShell Script
+
+# Get list of mp4 files in the current directory
+Get-ChildItem -Filter "*.mp4" | ForEach-Object {
+    # Check if corresponding pdf exists
+    $pdf_name = [System.IO.Path]::GetFileNameWithoutExtension($_.Name) + ".pdf"
+    if (-Not (Test-Path $pdf_name)) {
+        # pdf doesn't exist, print the file name
+        Write-Host "Processing file: $($_.Name)"
+
+        # Run python script
+        python video2pdf.py $_.Name
+    }
+}
